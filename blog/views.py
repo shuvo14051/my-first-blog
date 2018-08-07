e4f13e3f82f073f2c8516aba5201cef4472cb69c
shuvo14051 <shuvo1137017@gmail.com>
2018-08-07 12:37:40 +0600

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request,'blog/index.html')
+def post_list(request):
+    return render(request,'blog/post_list.html',{})
